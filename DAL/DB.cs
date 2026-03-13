02a8d18d47c3562971ffb7a07fe8d8d2ef75e682
Skide <salahmesbeh@gmail.com>
2026-03-13 12:48:11 -0400

    a

--- DAL/DB.cs
+++ DAL/DB.cs
@@ -18,30 +18,24 @@ namespace DAL
 
 
 
-        public sealed class DB
-        {
-            #region singleton setup
-            private static readonly DB instance = new DB();
-            public static DB Instance { get { return instance; } }
-            #endregion
+        static public UsersRepository Users { get; set; }
+            = new UsersRepository();
 
-            static public UsersRepository Users { get; set; }
-                = new UsersRepository();
+        static public NotificationsRepository Notifications { get; set; }
+            = new NotificationsRepository();
 
-            static public NotificationsRepository Notifications { get; set; }
-                = new NotificationsRepository();
+        static public LoginsRepository Logins { get; set; }
+            = new LoginsRepository();
 
-            static public LoginsRepository Logins { get; set; }
-                = new LoginsRepository();
+        static public EventsRepository Events { get; set; }
+            = new EventsRepository();
 
-            static public EventsRepository Events { get; set; }
-                = new EventsRepository();
+        static public Repository<UnverifiedEmail> UnverifiedEmails { get; set; }
+            = new Repository<UnverifiedEmail>();
 
-            static public Repository<UnverifiedEmail> UnverifiedEmails { get; set; }
-                = new Repository<UnverifiedEmail>();
-
-            static public Repository<RenewPasswordCommand> RenewPasswordCommands { get; set; }
-                = new Repository<RenewPasswordCommand>();
-        }
+        static public Repository<RenewPasswordCommand> RenewPasswordCommands { get; set; }
+            = new Repository<RenewPasswordCommand>();
     }
+
+
 }
