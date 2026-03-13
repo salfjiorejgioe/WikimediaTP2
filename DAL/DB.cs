b055654cd40c38ec03a94c5b1f3514d17a9fe5cb
Skide <salahmesbeh@gmail.com>
2026-03-13 12:47:33 -0400

    Update DB.cs

--- DAL/DB.cs
+++ DAL/DB.cs
@@ -1,4 +1,5 @@
 ﻿using Models;
+using PhotosManager.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -15,5 +16,32 @@ namespace DAL
 
         public static MediasRepository Medias { get; set; } = new MediasRepository();
 
+
+
+        public sealed class DB
+        {
+            #region singleton setup
+            private static readonly DB instance = new DB();
+            public static DB Instance { get { return instance; } }
+            #endregion
+
+            static public UsersRepository Users { get; set; }
+                = new UsersRepository();
+
+            static public NotificationsRepository Notifications { get; set; }
+                = new NotificationsRepository();
+
+            static public LoginsRepository Logins { get; set; }
+                = new LoginsRepository();
+
+            static public EventsRepository Events { get; set; }
+                = new EventsRepository();
+
+            static public Repository<UnverifiedEmail> UnverifiedEmails { get; set; }
+                = new Repository<UnverifiedEmail>();
+
+            static public Repository<RenewPasswordCommand> RenewPasswordCommands { get; set; }
+                = new Repository<RenewPasswordCommand>();
+        }
     }
 }
