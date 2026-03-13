fa116c6ead07b4bf2675d7a1bd86f79f69e4f6d4
Skide <salahmesbeh@gmail.com>
2026-03-13 12:31:50 -0400

    xdlol

---
+++ Wikimedia-master/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Wikimedia
+{
+    public class BundleConfig
+    {
+        // Pour plus d'informations sur le regroupement, visitez https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/validation").Include(
+                            "~/Scripts/validation.js",
+                            "~/Scripts/jquery-maskedinput.js",
+                            "~/Scripts/bootbox.js",
+                            "~/Scripts/autoRefreshPanel.js",
+                            "~/Scripts/image-control.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/_layout.css",
+                        "~/Content/site.css",
+                        "~/Content/menu.css",
+                        "~/Content/media.css",
+                        "~/Content/image-control.css",
+                        "~/Content/jqui-custom-datepicker.css"));
+        }
+    }
+}
---
+++ Wikimedia-master/DAL/DB.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Hosting;
+
+namespace DAL
+{
+    public sealed class DB
+    {
+        #region singleton setup
+        private static readonly DB instance = new DB();
+        public static DB Instance { get { return instance; } }
+        #endregion
+
+        public static MediasRepository Medias { get; set; } = new MediasRepository();
+
+    }
+}
